814aedcdf7c9f1c640651412391d5103c55fa634
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2025-07-28 23:54:40 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# somnambulist
-Shows how to respond to user inactivity messages.
+# Somnambulist
+
+_**noun.** a person who walks around or performs other motor acts while asleep; sleepwalker._
+
+While watching TV, sometimes the viewer falls asleep, walks away... or both. To avoid consuming server resources while nobody is actually watching anything, after three hours of inactivity Senza will prepare to suspend the connection to the service. Before it does that, it will send a message to your app.
+
+This app shows how you can listen for a `userinactivity` event and display an message to the user asking if they are still there. If they are, they can press any button on the remote to dismiss the message and keep watching. 
+
+The app also shows how you can simulate this situation, so you don't have to wait three hours every time you want to test your app. Just press the back button (escape key) and after five seconds it will send a fake `userinactivity` message.
+
+## Interface
+
+* OK - toggle between foreground and background mode
+* Escape - simulate a user inactivity message in 5 seconds
+* Any button - dismiss the message
+
---
+++ index.js
@@ -0,0 +1,103 @@
+const url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+
+let player;
+
+window.addEventListener("load", async () => {
+  try {
+    await senza.init();
+
+		senza.lifecycle.addEventListener("userinactivity", (event) => {
+		  const timeout = event.detail?.timeout ?? 60;
+			console.log(`Got 'userinactivity' event with timeout of ${timeout} seconds.`);
+		  showMessage(timeout);
+		});
+
+    player = new senza.ShakaPlayer();
+    player.configure(playerConfig());
+    player.attach(video);
+    await player.load(url);
+    await video.play();
+
+	  senza.uiReady();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document.addEventListener("keydown", async function (event) {
+  if (overlay.style.display === "flex") {
+    hideMessage();
+  }
+
+  switch (event.key) {
+    case "Enter": await toggleBackground(); break;
+    case "Escape": await fakeUserInactivity(5, 30); break;
+    case "ArrowUp": break;
+    case "ArrowDown": break;
+    case "ArrowLeft": skip(-30); break;
+    case "ArrowRight": skip(30); break;
+    default: return;
+  }
+  event.preventDefault();
+});
+
+async function toggleBackground() {
+  if (senza.lifecycle.state == senza.lifecycle.UiState.BACKGROUND) {
+    await senza.lifecycle.moveToForeground();
+  } else {
+    await senza.lifecycle.moveToBackground();
+  }
+}
+
+async function playPause() {
+  if (video.paused) {
+    await video.play();
+  } else {
+    await video.pause();
+  }
+}
+
+function skip(seconds) {
+  video.currentTime = video.currentTime + seconds;
+}
+
+function playerConfig() {
+  return {abr: {restrictions: {maxHeight: 1080}}};
+}
+
+let countdownInterval = null;
+async function showMessage(seconds) {
+  if (senza.lifecycle.state == senza.lifecycle.UiState.BACKGROUND) {
+    await senza.lifecycle.moveToForeground();
+  }
+	
+	let remaining = seconds;
+  updateMessageText(remaining);
+  overlay.style.display = "flex";
+
+  countdownInterval = setInterval(() => {
+    remaining--;
+    if (remaining <= 0) {
+      hideMessage();
+    } else {
+      updateMessageText(remaining);
+    }
+  }, 1000);
+}
+
+function updateMessageText(remaining) {
+	message.innerHTML = `<b>Are you still there?</b><br><br>Press any button in<br>the next ${remaining} seconds<br>to keep watching.`;
+}
+
+function hideMessage() {
+  overlay.style.display = "none";
+  clearInterval(countdownInterval);
+}
+
+function fakeUserInactivity(delay = 5, timeout = 30) {
+	console.log(`Will send fake 'userinactivity' event in ${delay} seconds.`);
+  setTimeout(() => {
+		console.log("Sending fake 'userinactivity' event.");
+    senza.lifecycle.dispatchEvent(new CustomEvent("userinactivity", {detail: {timeout}}));
+  }, delay * 1000);
+}
